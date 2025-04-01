cd194346eb3145011ba492587d12f0ebbc257fb9
Mahdi Parastesh <fulcrum1378@gmail.com>
2025-04-01 12:56:05 +0330

    2025.04.01 - 4: implemented almost all methods in IranianDate

--- src/main/ir/mahdiparastesh/chrono/iranian/IranianChronology.java
+++ src/main/ir/mahdiparastesh/chrono/iranian/IranianChronology.java
@@ -5,25 +5,13 @@ import java.time.chrono.AbstractChronology;
 
 public class IranianChronology extends AbstractChronology implements Serializable {
 
-    /**
-     * Singleton instance for Iranian chronology.
-     */
     public static final IranianChronology INSTANCE = new IranianChronology();
 
-    /**
-     * Restricted constructor.
-     */
     private IranianChronology() {
     }
 
     /**
-     * Gets the ID of the chronology.
-     * <p>
-     * The ID uniquely identifies the {@code Chronology}.
-     * It can be used to lookup the {@code Chronology} using {@link #of(String)}.
-     *
-     * @return the chronology ID, not null
-     * @see #getCalendarType()
+     * {@inheritDoc}
      */
     @Override
     public String getId() {
@@ -31,17 +19,7 @@ public class IranianChronology extends AbstractChronology implements Serializabl
     }
 
     /**
-     * Gets the calendar type of the calendar system.
-     * <p>
-     * The calendar type is an identifier defined by the CLDR and
-     * <em>Unicode Locale Data Markup Language (LDML)</em> specifications
-     * to uniquely identify a calendar.
-     * The {@code getCalendarType} is the concatenation of the CLDR calendar type
-     * and the variant, if applicable, is appended separated by "-".
-     * The calendar type is used to lookup the {@code Chronology} using {@link #of(String)}.
-     *
-     * @return the calendar system type, null if the calendar is not defined by CLDR/LDML
-     * @see #getId()
+     * {@inheritDoc}
      */
     @Override
     public String getCalendarType() {
@@ -49,17 +27,10 @@ public class IranianChronology extends AbstractChronology implements Serializabl
     }
 
     /**
-     * Obtains a local date in this chronology from the proleptic-year,
-     * month-of-year and day-of-month fields.
-     *
-     * @param prolepticYear the chronology proleptic-year
-     * @param month         the chronology month-of-year
-     * @param dayOfMonth    the chronology day-of-month
-     * @return the local date in this chronology, not null
-     * @throws DateTimeException if unable to create the date
+     * {@inheritDoc}
      */
     @Override
     public IranianDate date(int prolepticYear, int month, int dayOfMonth) {
-        return null;
+        return null; // TODO
     }
 }
