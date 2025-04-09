31c171686b6e48ceefa488d6febb133836810343
Mahdi Parastesh <fulcrum1378@gmail.com>
2025-04-10 00:55:05 +0330

    2025.04.10 - 1: a PowerShell script for creating JARs

---
+++ jar.ps1
@@ -0,0 +1,4 @@
+Set-Location out/production/IranianChronology
+jar vcfe ../../jar/IranianChronology/iranian-chronology-1.0.jar -C `
+    ir/mahdiparastesh/chrono/IranianChronology*.class `
+    ir/mahdiparastesh/chrono/IranianDate*.class
