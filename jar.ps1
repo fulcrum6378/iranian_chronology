56ca38cbb478d381bf9fa3f65c4114e7ac8f2a82
Mahdi Parastesh <fulcrum1378@gmail.com>
2025-06-02 04:27:00 +0330

    2025.06.02 - 1: back to Java 24 (v24.0.1 this time)

--- jar.ps1
+++ jar.ps1
@@ -1,4 +1,4 @@
 Set-Location out/production/IranianChronology
-jar cfmv ../../jar/IranianChronology/iranian-chronology-1.1.jar ../../../META-INF/MANIFEST.MF `
+jar cfmv ../../jar/IranianChronology/iranian-chronology-1.1.jar ../../../MANIFEST.MF `
     ir/mahdiparastesh/chrono/*.class `
     ../../../LICENSE.txt `
